8fd8d7a02430e088fab44eb132027b8bd464de75
JasperZhao97 <zhaolei97@foxmail.com>
2019-07-31 22:59:46 +0800

    API基础 object的常用方法

--- src/com/demo/api/TestObjectMethod.java
+++ src/com/demo/api/TestObjectMethod.java
@@ -1,21 +1,34 @@
 package com.demo.api;
 
 /**
- * 了解object
+ * 学生类
  */
 class Student {
 
-    private String name;
-    private int age;
-    private String gender;
+    private String number; //学号
+    private String name; //姓名
+    private String gender; //性别
 
     public Student() {
+
     }
 
-    public Student(String name, int age, String gender) {
+    public Student(String number) {
+        this.number = number;
+    }
+
+    public Student(String number, String name, String gender) {
+        this.number = number;
         this.name = name;
-        this.age = age;
         this.gender = gender;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 
     public String getName() {
@@ -26,12 +39,47 @@ class Student {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
+    public String getGender() {
+        return gender;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
+
+/**
+ * 人员类
+ */
+class Person {
+
+    private String number; //编号
+    private String name; //姓名
+    private String gender; //性别
+
+    public Person() {
+    }
+
+    public Person(String number, String name, String gender) {
+        this.number = number;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getGender() {
@@ -44,23 +92,47 @@ class Student {
 
     @Override
     public String toString() {
-        return "";
+        return number+" "+name+" "+gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //return this.number.equals(((Person) o).getNumber());
+        Person p = (Person) o;
+        return this.number.equals(p.getNumber());
     }
 }
+
 /**
+ * @author Teacher
  * @version 1.0.0
- * @auther ZhaoLei
- * @creatTime 2019年07月31日  13:58:53
+ * @createTime 2019年07月31日 13:39:37
  */
 public class TestObjectMethod {
 
     public static void main(String[] args) {
-        Student student1 = new Student("李三",20,"女");
-        Student student2 = new Student("李三",20,"女");
-        System.out.println(student1);
-        System.out.println(student2);
+        /*Student student1 = new Student("00001", "张山", "男");
+        Student student2 = new Student("00001", "李丽", "女");
+        System.out.println(student1+"  "+student1.toString());
+        System.out.println(student2+"  "+student2.toString());
+        System.out.println("--------------------------------");
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
+        System.out.println("--------------------------------");
         System.out.println(student1==student2);
         System.out.println(student1.equals(student2));
-//        System.out.println(student.hashCode());
+
+        //int i = 0x1b6d3586;
+        System.out.println(0X1b6d3586); //460141958*/
+
+        Person p1 = new Person("0001", "张山", "男");
+        Person p2 = new Person("0001", "李师师", "女");
+        Person p3 = new Person("0002", "王武", "男");
+        System.out.println(p1);
+        System.out.println(p1==p2);
+        System.out.println(p1.equals(p2));
+        System.out.println(p1==p3);
+        System.out.println(p1.equals(p3));
     }
+
 }
